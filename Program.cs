506d96896072a74b1c161a542677106ec3a62f34
Afonso -aptsharp <aff-on@hotmail.com>
2019-12-26 14:15:15 -0300

    Metodo não viavel. 2 linhas para fazer a estanciação do arquivo.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Manip.Arq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\aff-o\source\repos\Manip.Arq\Msg.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                //duas linhas de codigo não são viaveis para fazer a instanciação
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("ERROR");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Manip.Arq---FileStream-e-Stream-Reader
+#Recursos de entrada de saida apartir de um objeto com entrada e manupulação binaria. 
